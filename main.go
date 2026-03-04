5f5538223bf36ef0be149a0a3ba71ee8ee215bdd
zainfikrizain <zainfikrizain@gmail.com>
2026-03-05 05:40:55 +0700

    first

---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"Tugas-2/database"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+// Struktur data (Model)
+type Produk struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json: "descrciption"`
+}
+
+var produk = []Produk{
+	{ID: 1, Name: "Indomie Godog", Description: "{deskripsi 1}"},
+	{ID: 2, Name: "Vit 1000ml", Description: "{deskripsi 2}"},
+	{ID: 3, Name: "kecap", Description: "{deskripsi 3}"},
+}
+
+// Handler untuk API
+func getProdukByID(w http.ResponseWriter, r *http.Request) {
+	// Parse ID dari URL path
+	// URL: /api/produk/123 -> ID = 123
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/produk/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid Produk ID", http.StatusBadRequest)
+		return
+	}
+
+	// Cari produk dengan ID tersebut
+	for _, p := range produk {
+		if p.ID == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(p)
+			return
+		}
+	}
+
+	// Kalau tidak found
+	http.Error(w, "Produk belum ada", http.StatusNotFound)
+}
+
+func updateProduk(w http.ResponseWriter, r *http.Request) {
+	// get id dari request
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/produk/")
+
+	// ganti int
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid Produk ID", http.StatusBadRequest)
+		return
+	}
+
+	// get data dari request
+	var updateProduk Produk
+	err = json.NewDecoder(r.Body).Decode(&updateProduk)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	// loop produk, cari id, ganti sesuai data dari request
+	for i := range produk {
+		if produk[i].ID == id {
+			updateProduk.ID = id
+			produk[i] = updateProduk
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(updateProduk)
+			return
+		}
+	}
+
+	http.Error(w, "Produk belum ada", http.StatusNotFound)
+}
+
+func deleteProduk(w http.ResponseWriter, r *http.Request) {
+	//get id
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/produk/")
+
+	//ganti id int
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid Produk ID", http.StatusBadRequest)
+		return
+	}
+
+	for i, p := range produk {
+		if p.ID == id {
+			// bikin slice baru dengan data sebelum dan sesudah index
+			produk = append(produk[:i], produk[i+1:]...)
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(map[string]string{
+				"message": "sukses delete",
+			})
+			return
+		}
+	}
+	http.Error(w, "Produdk Belum Ada", http.StatusNotFound)
+}
+
+type Config struct {
+	Port    string `mapstructure: "PORT"`
+	DB_CONN string `mapstructure: "DB_CONN"`
+}
+
+func main() {
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(",", "_"))
+	if _, err := os.Stat(".env"); err == nil {
+		viper.SetConfigFile(".env")
+		_ = viper.ReadInConfig()
+	}
+
+	config := Config{
+		Port:    viper.GetString("PORT"),
+		DB_CONN: viper.GetString("DB_CONN"),
+	}
+
+	db, err := database.InitDB(config.DB_CONN)
+	if err != nil {
+		log.Fatal("Failed to initialize database:", err)
+	}
+	defer db.Close()
+
+}
